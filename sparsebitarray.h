dd48133ddf7f8fafb4651d40e1d36c13e1521528
timo <timo@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2007-11-23 08:17:57 +0000

    A new, untested, uncompiled version the sparse bit array. I just want to save my progress.
    git-svn-id: file:///var/svn/nexopia/trunk/usersearch@11555 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- sparsebitarray.h
+++ sparsebitarray.h
@@ -6,36 +6,15 @@
 
 using namespace std;
 
-#include <vector>
-#include "search.h"
+#include <string>
 
 /***********
- * The rledata has the format:
+ * The rledata is formed as a series of blocks. Each block has the format:
  * - top bit represents what this block is encoding (ie run of 0s or 1s)
  * - next 2 bits represent the length of this block, in terms of additional bytes
  *    (ie 0,1,2,3 additional bytes, for a total of 1,2,3,4 bytes)
  * - remainder of the block (varying between 5 bits and 29 bits) represents the run length
  **********/
-
-
-
-/****************
- * The rledata is made as a series of frames. Each frame will start with a keyframe
- * which specifies the initial offset, followed by an arbitrary number of blocks.
- * The top bit will be used to flag the start of a frame. Keyframes are 4 bytes in length,
- * using 31 bits to specify the offset.
- * The following blocks will have the format:
- * - top bit will be 0
- * - next bit will represent the value this block is encoding (a series of 1s or 0s)
- * - next 2 bits represent the length of this block in terms of additional bytes
- *     (ie 0,1,2,3 additional bytes, for a total of 1,2,3,4 bytes)
- * - The remaining 4 bits will be the top bits of the length
- * - In the 0-3 bytes that follow that are part of the block (according to the length)
- *     will have the top bit set to 0, and the remaining 7 bits will be the remaining bits of the length
- * - Any number of these blocks can follow a keyframe
- * - The keyframe offset plus the cumulative lengths of the blocks shall not exceed the value of the next keyframe
- * - Keyframes can be used to do semi-random access to the middle of the rledata
- ****************/
 
 class sparse_bit_array {
 	string rledata;
@@ -45,7 +24,11 @@ class sparse_bit_array {
 
 public:
 	sparse_bit_array(){
-		rledata = string();
+		rledata = "";
+	
+		rledata.append(block(0,1).encode());
+		rledata.append('\0'); //null terminate. \0 is equal to a 1 byte block of 0s of runlength 0
+
 		numitems = 0;
 	}
 
@@ -61,157 +44,150 @@ public:
 		return numitems;
 	}
 
-
 	iterator begin(){
-		return iterator.iterator(rledata);
+		return iterator(rledata.begin());
 	}
 
 	iterator end(){
-		return iterator.iterator(rledata + _size);
+		return iterator(rledata.end() - 1); //point to the last byte, which is a single byte block as a terminator
 	}
 
 private:
 	bool set(unsigned int index, unsigned int newval){
 
-
+		assert(newval == 0 || newval == 1);
+		assert(index > 0);
 
 
 /*
-		find frame
-			if no frame, create one at this offset
-		find block
-			if no block, create one or two?
-		if block value == newval
-			return false
-		if block length == 1
-			join prev and next blocks
-		else if index is first of the block
-			shorten this block
-			lengthen the prev block
-		else if(index is last of the block
-			lengthen this block
-			shorten next block
-		else
-			split block in 3
-			potentially introduce a keyframe
-*/		
+	find block
+	if no block found
+		create one or two
+	if block value == newval
+		return false
+	if block runlength == 1
+		join prev and next blocks
+	else if index is first of the block
+		shorten this block
+		lengthen the prev block
+	else if(index is last of the block
+		shorten this block
+		lengthen next block
+	else
+		split block in 3
+*/
 
 
 
 		unsigned int curindex = 0;
-		frame curframe;
-		frame tempframe;
 
-		block curblock;
-		block prevblock;
+		block cur;
+		block prev;
+		block next;
 
 		string newblocks;
 
-		//create an initial frame at offset 0
-		if(rledata.empty()){
-			tempframe = frame(0);
-			rledata.append(tempframe.encode());
-		}
-
 		string::iterator ptr = rledata.begin();
-		string::iterator end = rledata.end();
 
-	//find the right frame
-		curframe = frame(ptr);
 
-		while(ptr != end){
-			if(*ptr & 0x80){
-				tempframe = frame(ptr);
-				if(tempframe.value > index) //if we've found the next frame, stop
-					break;
-				curframe = tempframe;
-			}
-			++ptr;
-		}
-	
 	//find the right block
-		ptr = curframe.ptr + 4; // point to the first block in the frame
-		curindex = curframe.value;
-		
+		cur = block(ptr, 0);
 
-		while(ptr != end && !(*ptr & 0x80)){ //not the end, and not the next frame
-			curblock = block(ptr, curindex);
+		while(cur.runlength){
+			if(cur.value + cur.runlength - 1 >= index) //found!
+				break;
+
+			prev = cur;
+			cur = cur.next();
+		}
+
+		
+	//at this point, cur will obviously always be set.
+	//prevblock is also always set, because of the head block of 0 set at offset 0
+	//this is always going to be true, since offset 0 is an illegal index to set
+
+
+	//got to the end without finding a block, create one or two
+		if(!cur.runlength){
+			newblocks = "";
+
+			if(prev.bitvalue == newval){ //need a separator block
+				newblocks.append(
+					block(
+						(1 - newval), //bit value
+						(index - (prev.value + runlength))
+						)
+					);
+			}
+			newblocks.append(block(newval, 1));
 			
-		
-		
+			rledata.insert(rledata.size()-2, newblocks);
+			return true;
 		}
+
+
+	//already set
+		if(cur.bitvalue == newval)
+			return false
+
+
 		
-		
-		
-		/*
-		 * Possible operations on a set (set to 0 or 1)
-		 * - Already set
-		 * - Add to the end:
-		 *   - create a new block or two (key frame?)
-		 * - Add to the end of a block      ie 100011 => 100111
-		 * - Add to the begining of a block ie 100111 => 100011
-		 *   - Update the lengths of the two bordering blocks
-		 *
-		 * - Split a block in three         ie 100011 => 101011
-		 *   - Create 2 new blocks
-		 *   - Update the length of the existing block
-		 * - Join three blocks              ie 101011 => 100011
-		 *   - Update the length of the first block
-		 *   - Remove the extra blocks		 
-		 *
-		 * If block lengths change (ie run length could be represented with a shorter block)
-		 *   should it be shortened?
-		 *		 
-		 * Return whether a change was made or not
-		 */
-		
-		
-		
+	//if block runlength == 1
+	//	join prev and next blocks
+		if(cur.runlength == 1){
+			next = cur.next();
+
+			newblocks = block(newval, prev.runlength + 1 + next.runlength).encode();
+
+			rledata.replace(prev.rledata, next.rledata + next.blocklength - 1, newblocks);
+			return true;
+		}
+
+	//else if index is first of the block
+	//	shorten this block
+	//	lengthen the prev block
+		if(cur.value == index){
+			cur.runlength--;
+			prev.runlength++;
+
+			newblocks = prev.encode() + cur.encode();
+
+			rledata.replace(pre.rledata, cur.rledata + cur.blocklength - 1, newblocks);
+
+			return true;
+		}
+
+	//else if(index is last of the block
+	//	shorten this block
+	//	lengthen next block
+		if(cur.value + cur.runlength - 1 == index){
+			next = cur.next();
+
+			cur.runlength--;
+			next.runlength++;
+
+			newblocks = cur.encode() + next.encode();
+
+			rledata.replace(cur.rledata, next.rledata + next.blocklength - 1, newblocks);
+
+			return true;
+		}
+
+	//else
+	//	split block in 3
+		newblocks = block(cur.bitvalue, cur.value - index) +
+					block(newval, 1) +
+					block(cur.bitvalue, cur.value + cur.runlength - 1);
+
+		rledata.replace(cur.rledata, cur.rledata + cur.blocklength - 1, newblocks);
+		return true;
 	}
 
-
-	struct frame {
-		unsigned int value; //value of the start index of this frame
-		unsigned int numblocks;
-		string::iterator ptr;
-	
-		frame(unsigned int _value){
-			value = _value;
-			assert(value & (1 << 31) == 0); //only takes 31 bits
-		}
-
-		frame(string::iterator _ptr){
-			ptr = _ptr;
-
-			assert(ptr & (1 << 7) != 0); //top bit is set
-
-			value = 0;
-			value |= (*(ptr) & 0x7F) << 24;
-			value |= (*(ptr + 1) << 16;
-			value |= (*(ptr + 2) <<  8;
-			value |= (*(ptr + 3) <<  0;
-		}
-
-		string encode(){
-			assert(value & (1 << 31) == 0); //only takes 31 bits
-
-			string str = string(4, '\0');
-
-			str[0] = (value & (0x7F << 24)) >> 24;
-			str[1] = (value & (0xFF << 16)) >> 16;
-			str[2] = (value & (0xFF <<  8)) >>  8;
-			str[3] = (value & (0xFF <<  0)) >>  0;
-
-			str[0] |= 0x80; //set the top bit
-
-			return str;
-		}
-	}
 
 	struct block {
 		unsigned int value;        //value of the key (ie offset in the bit array)
 		unsigned int bitvalue;     //value of the bits in the run
-		unsigned int blocklength;  //length of memory used by this block (0-3 bytes)
+		unsigned int blocklength;  //length of memory used by this block (1-4 bytes)
 		unsigned int runlength;    //run length
 		string::iterator rledata;  //iterator to start of this block
 
@@ -221,84 +197,117 @@ private:
 			blocklength = 0;
 			runlength = 0;
 		}
+		
+		block(unsigned int _bitvalue, unsigned int _runlength){
+			value = 0;
+			blocklength = 0;
+
+			bitvalue = _bitvalue;
+			runlength = _runlength;
+		}
 
 		block(string::iterator _rledata, unsigned int _value){
 			rledata = _rledata;
 			value   = _value;
 
-			assert((*rledata & 0x80) != 0x80); //not a keyframe
-
-			bitvalue    = ((*rledata & 0x40) >> 6); //grab the 7th bit
-			blocklength = ((*rledata & 0x30) >> 4); //grab the 5th and 6th bit
+			bitvalue    = ((*rledata & (1 << 7)) >> 7);     //grab the 7th bit
+			blocklength = ((*rledata & (3 << 5)) >> 5) + 1; //grab the 5th and 6th bit, +1 to give the full block length
 
 			runlength = 0;
 
 			switch(blocklength){ // acts as an unrolled loop
-				case 3: runlength |= ((*(rledata + 3) & 0x7F) << 18);
-				case 2: runlength |= ((*(rledata + 2) & 0x7F) << 11);
-				case 1: runlength |= ((*(rledata + 1) & 0x7F) <<  4);
-				case 0: runlength |= ((*(rledata    ) & 0x0F) <<  0);
+				case 4: runlength |= ((*(rledata + 3) & 0xFF) << 21);
+				case 3: runlength |= ((*(rledata + 2) & 0xFF) << 13);
+				case 2: runlength |= ((*(rledata + 1) & 0xFF) <<  5);
+				case 1: runlength |= ((*(rledata    ) & 0x1F) <<  0);
 			}
 		}
 
 		string encode(){
 			string ret;
 
-			if(runlength <= 0x0F)	      blocklength = 0; // 4 bits of 1s
-			else if(runlength <= 0x7FF)   blocklength = 1; //11 bits of 1s
-			else if(runlength <= 0x3FFFF) blocklength = 2; //18 bits of 1s
-			else                          blocklength = 3;
+			assert( bitvalue    >= 0 && bitvalue    <= 1);
+			assert( blocklength >= 1 && blocklength <= 4);
+			assert( runlength   >= 1 && runlength   <= ((1 << 29) - 1)); //max of 29 bits long
 
-			ret = string(runlength+1, '\0');
+			if(     runlength <= 0x00001F) blocklength = 1; // 5 bits of 1s
+			else if(runlength <= 0x001FFF) blocklength = 2; //13 bits of 1s
+			else if(runlength <= 0x1FFFFF) blocklength = 3; //21 bits of 1s
+			else                           blocklength = 4;
 
-			ret[0] = (char) (((bitvalue & 1) << 6) | (blocklength << 4)); //just the block header, ie bit value and block length
+			ret = string(blocklength, '\0');
+
+			ret[0] = 0;
+			ret[0] |= ((bitvalue & 1) << 7);    //store the bit value
+			ret[0] |= ((blocklength - 1) << 5); //store block length. The -1 is to store 1-4 as 0-3 so it fits in 2 bits
+
 			switch(blocklength){
-				case 3: ret[3] |= ((runlength & (0x7F << 18)) >> 18);
-				case 2: ret[2] |= ((runlength & (0x7F << 11)) >> 11);
-				case 1: ret[1] |= ((runlength & (0x7F <<  4)) >>  4);
-				case 0: ret[0] |= ((runlength & (0x0F <<  0)) >>  0);
+				case 4: ret[3] |= ((runlength & (0x7F << 21)) >> 21);
+				case 3: ret[2] |= ((runlength & (0x7F << 13)) >> 13);
+				case 2: ret[1] |= ((runlength & (0x7F <<  5)) >>  5);
+				case 1: ret[0] |= ((runlength & (0x0F <<  0)) >>  0);
 			}
 
 			return ret;
 		}
-	}
+		
+		block next(){
+			return block(rledata + blocklength, value + runlength);
+		}
+	};
 
 
 	class iterator {
-
-		unsigned int value;
-
-		unsigned int datapos; //which block I'm on
-		unsigned int blockpos;
+		block cur;
+		unsigned int progress;
 		
-		string * rledata;
-		
-		iterator(string * data){
-			rledata = data;
-
-			datapos = 0;
-			blockpos = 0;
-
-			value = 0;
+		iterator(string::iterator rledata){
+			cur = block(rledata, 0);
+			progress = 0;
+			done = 0;
 		}
-		
+
+		iterator(const iterator it){
+			cur = it.cur;
+			progress = it.progress;
+		}
+
 		const unsigned int operator *() const {
-			return value;
+			return cur.value + progress;
 		}
-		
+
+		bool done(){
+			return (cur.runlength == 0);
+		}
+
+		bool operator == (const iterator & rhs) const {
+			return (cur.value == rhs.cur.value && cur.progress == rhs.cur.progress);
+		}
+
+		bool operator != (const iterator & rhs) const {
+			return !(this == rhs);
+		}
+
 		iterator operator ++(){ //prefix form
-			
+			progress++;
+
+		//move forward if
+		//   this isn't the terminator block and
+		//   this is a block of 0s, or done walking this block of 1s
+			while(cur.runlength && (!cur.bitvalue || progress >= cur.runlength)){
+				cur = block(cur.rledata + cur.blocklength, cur.value + cur.runlength);
+				progress = 0;
+			}
+
+			return *this;
 		}
 
-
-/*		
 		iterator operator ++(int){ //postfix form
-
+			iterator newit(this);
+			++this;
+			return newit;
 		}
-*/
-		
-	
-	}
+	};
 };
 
 #endif
