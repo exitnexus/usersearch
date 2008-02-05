4255e73dc2375ee18c62964d153c9ec79c051ddd
timo <timo@7be605d3-0650-4ba7-bb8c-0af3a60b5258>
2008-02-05 03:23:02 +0000

    Fixed up the user dump api
    git-svn-id: file:///var/svn/nexopia/trunk/usersearch@12867 7be605d3-0650-4ba7-bb8c-0af3a60b5258

--- gendata.php
+++ gendata.php
@@ -1,25 +1,78 @@
 <?
 
 	$forceserver = 1;
-	chdir("/home/nexopia/public_html/");
+//	@chdir("/home/nexopia/public_html/");
 	require_once("include/general.lib.php");
 
-	$res = $usersdb->unbuffered_query("SELECT * FROM usersearch");
+	set_time_limit(0);
 
-	$line = $res->fetchrow();
-	echo implode(",", array_keys($line)) . "\n";
-	do{
-		$line['sex'] = ($line['sex'] == 'Male' ? 0 : 1);
-		echo implode(",", $line) . "\n";
-	}while($line = $res->fetchrow());
+	$ip = getip();
+	if(substr($ip, 0, 3) != "10.")
+		die("Permission Denied");
 
-	echo "\n";
+	header("Content-type", "text/text");
 
-	$res = $usersdb->unbuffered_query("SELECT * FROM userinterests");
+	$dumpdbs = $usersdb->getSplitDBs();
 
-	$line = $res->fetchrow();
-	echo implode(",", array_keys($line)) . "\n";
-	do{
-		echo implode(",", $line) . "\n";
-	}while($line = $res->fetchrow());
+	if(getREQval('small', 'bool'))
+		$dumpdbs = array($dumpdbs[5]);
+	
+	switch($action){
+		case "locations":
+			echo "id,parent\n";
+
+			$res = $configdb->query("SELECT id, parent FROM locations");
+			
+			while($line = $res->fetchrow())
+				echo implode(",", $line) . "\n";
+
+			break;
+
+		case "users":
+			echo "userid,age,sex,loc,active,pic,single,sexuality\n";
+
+			$query = $usersdb->prepare("SELECT userid, age, IF(sex = 'Male', 0, 1) as sex, loc, ( (activetime > ?) + (activetime > ?) + (online = 'y' && activetime > ?) ) as active, ( (firstpic >= 1) + (signpic = 'y') ) as pic, (single = 'y') as single, sexuality FROM users", 
+				time() - 86400*30, time() - 86400*7, time() - 600);
+
+			$str = "";
+
+			foreach($dumpdbs as $userdb){
+				$res = $userdb->unbuffered_query($query);
+
+				while($line = $res->fetchrow()){
+					$str .= implode(",", $line) . "\n";
+					if(strlen($str) > 10000){
+						echo $str;
+						$str = "";
+					}
+				}
+				if($small)
+					break;
+			}
+			echo $str;
+
+			break;
+
+		case "interests":
+			echo "userid,interestid\n";
+
+			$str = "";
+
+			foreach($dumpdbs as $userdb){
+				$res = $userdb->unbuffered_query("SELECT userid,interestid FROM userinterests");
+
+				while($line = $res->fetchrow()){
+					$str .= implode(",", $line) . "\n";
+					if(strlen($str) > 10000){
+						echo $str;
+						$str = "";
+					}
+				}
+				if($small)
+					break;
+			}
+			echo $str;
+
+			break;
+	}
 
